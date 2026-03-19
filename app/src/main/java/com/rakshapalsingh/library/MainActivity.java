b4993848742e06eefa5e5d1b58a71b1a3a852eef
CdoAnujKumar <ususikarwar.02@gmail.com>
2026-03-19 14:39:04 +0530

    Update MainActivity.java

--- app/src/main/java/com/rakshapalsingh/library/MainActivity.java
+++ app/src/main/java/com/rakshapalsingh/library/MainActivity.java
@@ -96,9 +96,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Enable Chrome DevTools debugging in debug builds
-        if (BuildConfig.DEBUG) {
+        
             WebView.setWebContentsDebuggingEnabled(true);
-        }
+        
 
         webView.setWebChromeClient(new WebChromeClient());
     }
